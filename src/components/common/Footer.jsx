600c21412c510572c094b6c4c66e5f61211ffd56
Happy Kumar <happykumar.info@gmail.com>
2026-03-26 17:29:51 +0530

    initial commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,35 @@
+import * as Route from '@/constants/routes';
+import logo from '@/images/logo-full.png';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Footer = () => {
+  const { pathname } = useLocation();
+
+  const visibleOnlyPath = [
+    Route.HOME,
+    Route.SHOP
+  ];
+
+  return !visibleOnlyPath.includes(pathname) ? null : (
+    <footer className="footer">
+      <div className="footer-col-1">
+        <strong>
+          <span>
+            Developed by
+            {' '}
+            <a href="https://happykumar.com">Happy Kumar</a>
+          </span>
+        </strong>
+      </div>
+      <div className="footer-col-2">
+        <img alt="Footer logo" className="footer-logo" src={logo} />
+        <h5>
+          &copy;&nbsp; {new Date().getFullYear()} EyeKart. All rights reserved.
+        </h5>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
